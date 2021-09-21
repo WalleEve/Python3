eb5315412fa459baa5e06d942182b067bddad3a6
WalleEve <42144097+WalleEve@users.noreply.github.com>
2021-09-21 08:56:35 +0530

    21 Sep 2021 Loop

--- Run.py
+++ Run.py
@@ -1,122 +1,139 @@
-# LOOPING 
+# 
 
-num1 = int(input("Enter first number"))
-num2 = int(input("Enter second number"))
+usr_input = "uma"
 
-if num1 > num2:
-    print(f"{num1} is greatest number")
-else:
-    print(f"{num2} is greatest number")
-    
-
-LOOP 
-
-difinite loop :
-    when we know how many number if of times the statement will execute 
-
-indefinite loop:
+output: 
+    positive index [0] negative index [-3] | value: [u]
+    positive index [1] negative index [-2] | value: [m]
+    positive index [2] negative index [-1] | value: [a]
     
     
-while:
 
-for: 
+user_input = input("Enter a string: ")
+
+for i in range(len(user_input)):
+    print(f"positive index {i} negative index {( len(user_input) - i ) *-1 } | value: {user_input[i]}")
+
+# INFINITY LOOPS: 
+
     
-# difinite loop 
+a = '' # None
+while a != 'uma': # '' != uma # True 
+    input("enter your name") # sadhana 
 
 
+print(name)
 
-# While: 
+a = 'uma'
+
+while a:
+    print(a)
     
-condition: True 
-    statement 1
-    statement 2
-    statement 3
-    .
-    .
+
+# Nested Loop: 
+    
+If we take a loop inside another loop, then this will be called as nested loop.
+
+syntax: 
+    
+for <variable> in <iterative value>:
+    statement_1
+    for <variable> in <iterative value>:
+        statement_1
+        statement_2
     
 
- # syntax: 
-    
-while expression/condition:
-    statements 
-    .
-    .
-    
-print(1)
-print(2)
-print(3)
-print(4)
-print(5)
-
-count = 1 
-
-while count:
-    print(count+1)
-    print(count +1)
-    count = False 
-
-
-count = 1 
-
-while count <=5: # False 
-    print(count) # 1 , 2, 3, 4, 5
-    count += 1   # 6
-    
-    
-# indifinite loop
-
-flag = True 
-
-while flag:
-    a = int(input("Enter first number: "))
-    b = int(input("Enter second number: "))
-    
-    if a > b:
-        print(f"{a} is the greatest value")
-    else:
-        print(f"{b} is the greatest value")
+while <condition>:
+    statemnt_1 
+    while <condition>:
+        statement_1
+        statement_2 
         
-    print("Do you want to run this one more time:>")
-    u_input = input("Please choose[yes/no]")
-    if u_input.lower() == 'no':
-        flag = False 
+    
+
+
+for i in range(4): # 0, 1, 2, 3
+    for j in range(4): # 0, 1, 2, 3
+        print(f"i={i}  j={j}")
+              
+              
+cartesian product 
+
+1   4 5 6
+2   4 5 6
+3   4 5 6           
         
+ui = 4
+# 
 
+*
+* *
+* * * 
+* * * *
+
+n = int(input("Enter a number: "))
+
+for i in range(1, n+1): # 1 to n-1
+    for j in range(1, i+1):
+        print("*", end = ' ')
+    print()
+    
+
+
+n = int(input("Enter a number: "))
+for i in range(1, n+1): # 1, 2, 3, 4
+    print('* ' * i)
+    
+    
+
+
+
+
+
+
+
+# 
+ui = 4
+
+          *
+         * *
+        * * *
+       * * * *
+       
+
+n = int(input("Enter a number: ")) 
+
+for i in range(1, n+1):
+    print(' ' * (n-i), end='' ) 
+    print('* ' * i)
+
+
+#  Transfer statement :
+
+1. Break 
+2. continue 
+else 
+3. Pass 
+
+     
+       
  
-# For statement / loop 
+  
 
 
-Iteratable : strin, tuple, list, dic, set , range() 
 
 
-syntax:
-    
-for iterating var / var in sequence/ iteratble item:
-    
-    
-
-l = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-
-for i in l:
-    print(i)
-
- 
-l = ['a', 'b', 'c', 'd', 'e', 'f', 'g']    
-v = 0
-
-while v < len(l):
-    print(l[v])
-    v = v+1
-    
 
 
-l = list("1234")
-l[0] = l[1] = 5
-print(l)     
-    
-    
-    
 
-    
+
+
+
+
+
+
+
+
+
 
     
